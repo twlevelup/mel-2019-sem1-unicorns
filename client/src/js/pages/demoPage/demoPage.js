5d23e84d015fa70f26da676c06c8a378d35b1641
Tengku Ummil Syazwina Zainab <s3575233@student.rmit.edu.au>
2019-05-01 10:41:49 +0000

    Completed Activity Details page

--- client/src/js/pages/demoPage/demoPage.js
+++ client/src/js/pages/demoPage/demoPage.js
@@ -2,6 +2,15 @@ const BasePage = require('watch-framework').BasePage;
 
 class DemoPage extends BasePage {
   template = require('./demoPage.hbs');
+
+  topButtonEvent() {
+     this.navigate('/');
+  }
+
+  bottomButtonEvent() {
+     this.navigate('/');
+  }
+
 }
 
 module.exports = DemoPage;
--- client/src/js/pages/demoPage/demoPage.spec.js
+++ client/src/js/pages/demoPage/demoPage.spec.js
@@ -1,6 +1,6 @@
 const DemoPage = require('./demoPage');
 
-describe('The Demo Page', () => {
+describe('DemoPage', () => {
   let watchFace;
   beforeEach(() => {
     document.body.innerHTML = `<div id='watch-face' style='height: 100px; width: 100px;'></div>`;
@@ -10,7 +10,33 @@ describe('The Demo Page', () => {
   describe('#render', () => {
     it('should contain the correct text', () => {
       const page = new DemoPage();
-      expect(page.render()).toContain('This is a demo');
+      expect(page.render()).toContain('<h2 style="text-align:center; padding-bottom:30px; padding-right:25px;">Snooze</h2>');
+      expect(page.render()).toContain('<h1 style="text-align:center; padding-right:25px;">Take dog for a walk</h1>');
+      expect(page.render()).toContain('<h2 style="text-align:center; padding-top:30px; padding-right:25px;">Done</h2>');
     });
   });
+
+  describe('#topButtonEvent', () => {
+    it('goes to root page', () => {
+      const page = new DemoPage();
+      spyOn(page, 'navigate');
+
+      page.topButtonEvent();
+      expect(page.navigate).toHaveBeenCalledWith('/');
+    });
+  });
+
+   describe('#bottomButtonEvent', () => {
+    it('goes to root page', () => {
+      const page = new DemoPage();
+      spyOn(page, 'navigate');
+
+      page.bottomButtonEvent();
+      expect(page.navigate).toHaveBeenCalledWith('/');
+    });
+  });
+
 });
+
+
+
