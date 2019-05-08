fcee89c4fa1f1cd24a8b8f894a4eaa37cdc3ab29
knignemo97 <cookyjnr@gmail.com>
2019-05-08 10:51:13 +0000

    created page to show all activities

--- client/src/js/pages/contactsPage/contactsPage.js
+++ client/src/js/pages/contactsPage/contactsPage.js
@@ -1,16 +1,37 @@
 const BasePage = require('watch-framework').BasePage;
 const StorageHub = require('watch-framework').StorageHub;
+const activities = require('./../data');
 
 class ContactsPage extends BasePage {
   template = require('./contactsPage.hbs');
 
   pageWillLoad() {
-    this.contacts = StorageHub.getData('contacts')
+    this.activity = activities[ContactsPage.staticProperty]
   }
 
   leftButtonEvent() {
-    this.navigate('/');
+    if (ContactsPage.staticProperty != 0) {
+      ContactsPage.staticProperty--;
+      this.navigate('/');
+      this.navigate('contacts');
+    } else {
+      this.navigate('/');
+    }
+  }
+
+  rightButtonEvent() {
+    if (ContactsPage.staticProperty == activities.length - 1) {
+      ContactsPage.staticProperty = 0
+      this.navigate('/');
+    } else {
+      ContactsPage.staticProperty++;
+      console.log(ContactsPage.staticProperty)
+      this.navigate('/');
+      this.navigate('contacts');
+    }
   }
 }
+
+ContactsPage.staticProperty = 0;
 
 module.exports = ContactsPage;
