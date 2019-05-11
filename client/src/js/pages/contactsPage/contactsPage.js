54aeb9776be838512ee2324cfa086d78d5967749
knignemo97 <cookyjnr@gmail.com>
2019-05-11 02:15:22 +0000

    Change time format

--- client/src/js/pages/contactsPage/contactsPage.js
+++ client/src/js/pages/contactsPage/contactsPage.js
@@ -13,23 +13,31 @@ class ContactsPage extends BasePage {
   }
 
   createTime() {
-    var minutes = Math.floor((this.activity.time.getTime() - (new Date().getTime()))/60000)
-    if(minutes < 0) {
-      var string = 'Overdue in '
+    var minutes = (this.activity.time.getTime() - (new Date().getTime())) / 60000
+    if (minutes < 0) {
+      var string = 'Overdue '
       minutes *= -1
+      var endString = " ago"
     }
-      else {
+    else {
       var string = 'Due in '
-      }
-    if(minutes >= 60)
-      var hours = Math.floor(minutes/60)
-    else
-      return string +  minutes + ' minutes'
+      var endString = ""
+    }
 
-    if(hours >= 24)
-     return string + Math.floor(hours/24) + (Math.floor(hours/24)==1? ' day ' :  ' days ')
-    minutes = minutes - hours*60
-    return string + hours + (hours==1? ' hour ' :  ' hours and ') + minutes + (minutes==1? ' minute ' :  ' minutes ')
+    if (minutes < 1)
+      return string + Math.floor(minutes * 60) + (Math.floor(minutes * 60) == 1 ? ' second' : ' seconds') + endString
+
+    minutes = Math.floor(minutes)
+
+    if (minutes >= 60)
+      var hours = Math.floor(minutes / 60)
+    else
+      return string + minutes + ' minutes' + endString
+
+    if (hours >= 24)
+      return string + Math.floor(hours / 24) + (Math.floor(hours / 24) == 1 ? ' day ' : ' days ') + endString
+    minutes = minutes - hours * 60
+    return string + hours + (hours == 1 ? ' hour ' : ' hours and ') + minutes + (minutes == 1 ? ' minute ' : ' minutes ') + endString
   }
 
   leftButtonEvent() {
